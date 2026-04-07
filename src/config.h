c1218385551b2725475a966f768d29f195b7030f
Gavin <78394473+gi-b716@users.noreply.github.com>
2026-04-07 12:01:55 +0000

    Update versions

--- src/config.h
+++ src/config.h
@@ -15,7 +15,7 @@
 
 #define PRECOMPUTE48_BUFSIZ ((int64_t)1 << 30)
 
-enum { MC_DEFAULT = MC_1_21_3 };
+enum { MC_DEFAULT = MC_1_21_5 };
 
 struct ExtGenConfig
 {
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -696,7 +696,7 @@ void MainWindow::setMCList(bool experimental)
     {
         if (!experimental && mc != wi.mc)
         {
-            if (mc <= MC_1_0 || mc == MC_1_16_1 || mc == MC_1_19_2 || mc == MC_1_21_1 || mc == MC_1_21_WD)
+            if (mc <= MC_1_0 || mc == MC_1_16_1 || mc == MC_1_19_2 || mc == MC_1_21_1)
                 continue;
         }
         mclist.append(mc2str(mc));
